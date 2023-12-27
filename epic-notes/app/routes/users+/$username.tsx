34c78a3e626f5968f5358bb69cd73c400dc39ecd
Kobe Ruado <insidiae423@gmail.com>
2023-12-27 22:25:41 +0800

    Finish Exercise 5.4

--- epic-notes/app/routes/users+/$username.tsx
+++ epic-notes/app/routes/users+/$username.tsx
@@ -23,7 +23,7 @@ export default function ProfileRoute() {
 	return (
 		<div className="container mb-48 mt-36">
 			<h1 className="text-h1">{data.user.name ?? data.user.username}</h1>
-			<Link to="notes" className="underline">
+			<Link to="notes" className="underline" prefetch="intent">
 				Notes
 			</Link>
 		</div>
---
+++ full-stack-foundations/05.scripting/04.prefetching/$username.tsx
@@ -0,0 +1,33 @@
+import { json, type DataFunctionArgs } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+import { db } from '#app/utils/db.server.ts'
+import { invariantResponse } from '#app/utils/misc.tsx'
+
+export async function loader({ params }: DataFunctionArgs) {
+	const user = db.user.findFirst({
+		where: {
+			username: {
+				equals: params.username,
+			},
+		},
+	})
+
+	invariantResponse(user, 'User not found', { status: 404 })
+
+	return json({
+		user: { name: user.name, username: user.username },
+	})
+}
+
+export default function ProfileRoute() {
+	const data = useLoaderData<typeof loader>()
+	return (
+		<div className="container mb-48 mt-36">
+			<h1 className="text-h1">{data.user.name ?? data.user.username}</h1>
+			{/* 🐨 add a prefetch="intent" to this link */}
+			<Link to="notes" className="underline" prefetch="intent">
+				Notes
+			</Link>
+		</div>
+	)
+}
