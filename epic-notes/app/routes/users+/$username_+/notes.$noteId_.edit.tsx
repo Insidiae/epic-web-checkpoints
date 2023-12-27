14792d3ac145375267f6cf5f4a40f8174b53458e
Kobe Ruado <insidiae423@gmail.com>
2023-12-27 21:39:55 +0800

    Finish Exercise 4.1

--- epic-notes/app/routes/users+/$username_+/notes.$noteId_.edit.tsx
+++ epic-notes/app/routes/users+/$username_+/notes.$noteId_.edit.tsx
@@ -1,5 +1,10 @@
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
+import { floatingToolbarClassName } from "#app/components/floating-toolbar.tsx";
+import { Button } from "#app/components/ui/button.tsx";
+import { Input } from "#app/components/ui/input.tsx";
+import { Label } from "#app/components/ui/label.tsx";
+import { Textarea } from "#app/components/ui/textarea.tsx";
 import { db } from "#app/utils/db.server.ts";
 import { invariantResponse } from "#app/utils/misc.tsx";
 
@@ -22,5 +27,29 @@ export async function loader({ params }: LoaderFunctionArgs) {
 export default function NoteEdit() {
 	const data = useLoaderData<typeof loader>();
 
-	return <pre>{JSON.stringify(data, null, 2)}</pre>;
+	return (
+		<Form
+			method="POST"
+			className="flex h-full flex-col gap-y-4 overflow-x-hidden px-10 pb-28 pt-12"
+		>
+			<div className="flex flex-col gap-1">
+				<div>
+					{/* 🦉 NOTE: this is not an accessible label, we'll get to that in the accessibility exercises */}
+					<Label>Title</Label>
+					<Input name="title" defaultValue={data.note.title} />
+				</div>
+				<div>
+					{/* 🦉 NOTE: this is not an accessible label, we'll get to that in the accessibility exercises */}
+					<Label>Content</Label>
+					<Textarea name="content" defaultValue={data.note.content} />
+				</div>
+			</div>
+			<div className={floatingToolbarClassName}>
+				<Button variant="destructive" type="reset">
+					Reset
+				</Button>
+				<Button type="submit">Submit</Button>
+			</div>
+		</Form>
+	);
 }
