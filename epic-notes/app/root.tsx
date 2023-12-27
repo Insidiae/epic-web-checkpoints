a9124e75779af0a4899804f73249138c3599df83
Kobe Ruado <insidiae423@gmail.com>
2023-12-27 20:08:54 +0800

    Setup Exercise 2

--- epic-notes/app/root.tsx
+++ epic-notes/app/root.tsx
@@ -1,5 +1,13 @@
-import { type LinksFunction } from "@remix-run/node";
-import { Links, LiveReload, Scripts } from "@remix-run/react";
+import os from "node:os";
+import { cssBundleHref } from "@remix-run/css-bundle";
+import { json, type LinksFunction } from "@remix-run/node";
+import {
+	Links,
+	LiveReload,
+	Outlet,
+	Scripts,
+	useLoaderData,
+} from "@remix-run/react";
 import faviconAssetUrl from "./assets/favicon.svg";
 import fontStylesheetUrl from "./styles/font.css";
 import tailwindStylesheetUrl from "./styles/tailwind.css";
@@ -9,17 +17,44 @@ export const links: LinksFunction = () => {
 		{ rel: "icon", type: "image/svg+xml", href: faviconAssetUrl },
 		{ rel: "stylesheet", href: fontStylesheetUrl },
 		{ rel: "stylesheet", href: tailwindStylesheetUrl },
-	];
+		cssBundleHref ? { rel: "stylesheet", href: cssBundleHref } : null,
+	].filter(Boolean);
 };
 
+export async function loader() {
+	return json({ username: os.userInfo().username });
+}
+
 export default function App() {
+	const data = useLoaderData<typeof loader>();
+
 	return (
-		<html lang="en">
+		<html lang="en" className="h-full overflow-x-hidden">
 			<head>
 				<Links />
 			</head>
-			<body>
-				<p className="p-8 text-xl">Hello World</p>
+			<body className="flex h-full flex-col justify-between bg-background text-foreground">
+				<header className="container mx-auto py-6">
+					<nav className="flex justify-between">
+						<div>
+							<div className="font-light">epic</div>
+							<div className="font-bold">notes</div>
+						</div>
+					</nav>
+				</header>
+
+				<div className="flex-1">
+					<Outlet />
+				</div>
+
+				<div className="container mx-auto flex justify-between">
+					<div>
+						<div className="font-light">epic</div>
+						<div className="font-bold">notes</div>
+					</div>
+					<p>Built with ♥️ by {data.username}</p>
+				</div>
+				<div className="h-5" />
 				<Scripts />
 				<LiveReload />
 			</body>
