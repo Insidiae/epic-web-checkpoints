934fed1580ed1622954aa3c2c83ac390aa9ee3c2
Kobe Ruado <insidiae423@gmail.com>
2023-12-27 19:41:09 +0800

    Finish Exercise 1.2

--- epic-notes/app/root.tsx
+++ epic-notes/app/root.tsx
@@ -1,8 +1,9 @@
 import { type LinksFunction } from "@remix-run/node";
 import { Links, LiveReload, Scripts } from "@remix-run/react";
+import faviconAssetUrl from "./assets/favicon.svg";
 
 export const links: LinksFunction = () => {
-	return [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }];
+	return [{ rel: "icon", type: "image/svg+xml", href: faviconAssetUrl }];
 };
 
 export default function App() {
---
+++ full-stack-foundations/01.styling/02.asset-imports/root.tsx
@@ -0,0 +1,26 @@
+import { type LinksFunction } from "@remix-run/node";
+import { Links, LiveReload, Scripts } from "@remix-run/react";
+// 🐨 Import the SVG favicon, named as 'faviconAssetUrl', using a default import statement.
+import faviconAssetUrl from "./assets/favicon.svg";
+import { KCDShop } from "./kcdshop.tsx";
+
+export const links: LinksFunction = () => {
+  // 🐨 swap the hard-coded href here with the default import of the favicon
+  return [{ rel: "icon", type: "image/svg+xml", href: faviconAssetUrl }];
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Links />
+      </head>
+      <body>
+        <p>Hello World</p>
+        <Scripts />
+        <KCDShop />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
