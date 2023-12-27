aa5a55c2402de870ddab782d0ba9628cb6b42096
Kobe Ruado <insidiae423@gmail.com>
2023-12-27 22:06:44 +0800

    Finish Exercise 5.2

--- full-stack-foundations/05.scripting/02.scroll-restoration/notes.tsx
+++ full-stack-foundations/05.scripting/02.scroll-restoration/notes.tsx
@@ -1,36 +1,38 @@
-import { json, type LoaderFunctionArgs } from "@remix-run/node";
-import { Link, NavLink, Outlet, useLoaderData } from "@remix-run/react";
-import { db } from "#app/utils/db.server.ts";
-import { cn, invariantResponse } from "#app/utils/misc.tsx";
+import { json, type DataFunctionArgs } from '@remix-run/node'
+import { Link, NavLink, Outlet, useLoaderData } from '@remix-run/react'
+import { db } from '#app/utils/db.server.ts'
+import { cn, invariantResponse } from '#app/utils/misc.tsx'
 
-export function loader({ params }: LoaderFunctionArgs) {
+export async function loader({ params }: DataFunctionArgs) {
 	const owner = db.user.findFirst({
 		where: {
-			username: { equals: params.username },
+			username: {
+				equals: params.username,
+			},
 		},
-	});
+	})
 
-	invariantResponse(owner, "Owner not found", { status: 404 });
+	invariantResponse(owner, 'Owner not found', { status: 404 })
 
 	const notes = db.note
 		.findMany({
 			where: {
 				owner: {
-					username: { equals: params.username },
+					username: {
+						equals: params.username,
+					},
 				},
 			},
 		})
-		.map(({ id, title }) => ({ id, title }));
-
-	return json({ owner, notes });
+		.map(({ id, title }) => ({ id, title }))
+	return json({ owner, notes })
 }
 
 export default function NotesRoute() {
-	const data = useLoaderData<typeof loader>();
-
-	const ownerDisplayName = data.owner.name ?? data.owner.username;
+	const data = useLoaderData<typeof loader>()
+	const ownerDisplayName = data.owner.name ?? data.owner.username
 	const navLinkDefaultClassName =
-		"line-clamp-2 block rounded-l-full py-2 pl-8 pr-6 text-base lg:text-xl";
+		'line-clamp-2 block rounded-l-full py-2 pl-8 pr-6 text-base lg:text-xl'
 	return (
 		<main className="container flex h-full min-h-[400px] pb-12 px-0 md:px-8">
 			<div className="grid w-full grid-cols-4 bg-muted pl-2 md:container md:mx-2 md:rounded-3xl md:pr-0">
@@ -46,9 +48,10 @@ export default function NotesRoute() {
 								<li key={note.id} className="p-1 pr-0">
 									<NavLink
 										to={note.id}
+										// 🐨 add preventScrollReset to the NavLink
 										preventScrollReset
 										className={({ isActive }) =>
-											cn(navLinkDefaultClassName, isActive && "bg-accent")
+											cn(navLinkDefaultClassName, isActive && 'bg-accent')
 										}
 									>
 										{note.title}
@@ -63,5 +66,5 @@ export default function NotesRoute() {
 				</div>
 			</div>
 		</main>
-	);
+	)
 }
--- full-stack-foundations/05.scripting/02.scroll-restoration/root.tsx
+++ full-stack-foundations/05.scripting/02.scroll-restoration/root.tsx
@@ -1,6 +1,6 @@
-import os from "node:os";
-import { cssBundleHref } from "@remix-run/css-bundle";
-import { json, type LinksFunction } from "@remix-run/node";
+import os from 'node:os'
+import { cssBundleHref } from '@remix-run/css-bundle'
+import { json, type LinksFunction } from '@remix-run/node'
 import {
 	Link,
 	Links,
@@ -9,27 +9,27 @@ import {
 	Scripts,
 	ScrollRestoration,
 	useLoaderData,
-} from "@remix-run/react";
-import faviconAssetUrl from "./assets/favicon.svg";
-import fontStylesheetUrl from "./styles/font.css";
-import tailwindStylesheetUrl from "./styles/tailwind.css";
+} from '@remix-run/react'
+import faviconAssetUrl from './assets/favicon.svg'
+import { KCDShop } from './kcdshop.tsx'
+import fontStylesheetUrl from './styles/font.css'
+import tailwindStylesheetUrl from './styles/tailwind.css'
 
 export const links: LinksFunction = () => {
 	return [
-		{ rel: "icon", type: "image/svg+xml", href: faviconAssetUrl },
-		{ rel: "stylesheet", href: fontStylesheetUrl },
-		{ rel: "stylesheet", href: tailwindStylesheetUrl },
-		cssBundleHref ? { rel: "stylesheet", href: cssBundleHref } : null,
-	].filter(Boolean);
-};
+		{ rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl },
+		{ rel: 'stylesheet', href: fontStylesheetUrl },
+		{ rel: 'stylesheet', href: tailwindStylesheetUrl },
+		cssBundleHref ? { rel: 'stylesheet', href: cssBundleHref } : null,
+	].filter(Boolean)
+}
 
 export async function loader() {
-	return json({ username: os.userInfo().username });
+	return json({ username: os.userInfo().username })
 }
 
 export default function App() {
-	const data = useLoaderData<typeof loader>();
-
+	const data = useLoaderData<typeof loader>()
 	return (
 		<html lang="en" className="h-full overflow-x-hidden">
 			<head>
@@ -60,10 +60,12 @@ export default function App() {
 					<p>Built with ♥️ by {data.username}</p>
 				</div>
 				<div className="h-5" />
+				{/* 🐨 Add ScrollRestoration from '@remix-run/react' here */}
 				<ScrollRestoration />
 				<Scripts />
+				<KCDShop />
 				<LiveReload />
 			</body>
 		</html>
-	);
+	)
 }
