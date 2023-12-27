63d58a36fb22fdf07b71842d35b91f0726f7363b
Kobe Ruado <insidiae423@gmail.com>
2023-12-27 19:40:18 +0800

    Finish Exercise 1.1

--- epic-notes/app/root.tsx
+++ epic-notes/app/root.tsx
@@ -1,15 +1,16 @@
-import { LiveReload, Scripts } from "@remix-run/react";
+import { type LinksFunction } from "@remix-run/node";
+import { Links, LiveReload, Scripts } from "@remix-run/react";
 
-// 🐨 export a links function here that adds the favicon
-// 💰 It should have the following properties:
-// - rel: 'icon'
-// - type: 'image/svg+xml'
-// - href: '/favicon.svg'
+export const links: LinksFunction = () => {
+	return [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }];
+};
 
 export default function App() {
 	return (
 		<html lang="en">
-			<head>{/* 🐨 Put Remix's <Links /> in here */}</head>
+			<head>
+				<Links />
+			</head>
 			<body>
 				<p>Hello World</p>
 				<Scripts />
