170c1bd5f61ac964d720c587bba25b5805c31bf8
Kobe Ruado <insidiae423@gmail.com>
2023-12-27 20:25:31 +0800

    Finish Exercise 2.2

--- epic-notes/app/root.tsx
+++ epic-notes/app/root.tsx
@@ -2,6 +2,7 @@ import os from "node:os";
 import { cssBundleHref } from "@remix-run/css-bundle";
 import { json, type LinksFunction } from "@remix-run/node";
 import {
+	Link,
 	Links,
 	LiveReload,
 	Outlet,
@@ -36,10 +37,10 @@ export default function App() {
 			<body className="flex h-full flex-col justify-between bg-background text-foreground">
 				<header className="container mx-auto py-6">
 					<nav className="flex justify-between">
-						<div>
+						<Link to="/">
 							<div className="font-light">epic</div>
 							<div className="font-bold">notes</div>
-						</div>
+						</Link>
 					</nav>
 				</header>
 
@@ -48,10 +49,10 @@ export default function App() {
 				</div>
 
 				<div className="container mx-auto flex justify-between">
-					<div>
+					<Link to="/">
 						<div className="font-light">epic</div>
 						<div className="font-bold">notes</div>
-					</div>
+					</Link>
 					<p>Built with ♥️ by {data.username}</p>
 				</div>
 				<div className="h-5" />
--- epic-notes/app/routes/users+/kody_+/notes.tsx
+++ epic-notes/app/routes/users+/kody_+/notes.tsx
@@ -1,10 +1,30 @@
-import { Outlet } from '@remix-run/react'
+import { Link, NavLink, Outlet } from "@remix-run/react";
+import { cn } from "#app/utils/misc.tsx";
 
 export default function NotesRoute() {
 	return (
 		<div className="flex h-full justify-between pb-12 border-8 border-blue-500">
-			<h1 className="text-h1">Notes</h1>
+			<div>
+				<h1 className="text-h1">Notes</h1>
+				<ul>
+					<li>
+						<Link to=".." relative="path" className="underline">
+							Back to Kody
+						</Link>
+					</li>
+					<li>
+						<NavLink
+							to="some-note-id"
+							className={({ isActive }) =>
+								cn("underline", isActive && "bg-accent")
+							}
+						>
+							Some Note
+						</NavLink>
+					</li>
+				</ul>
+			</div>
 			<Outlet />
 		</div>
-	)
+	);
 }
