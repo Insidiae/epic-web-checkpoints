e2ca85641af332b7feb3fb712a318e0bcfb9dd18
Kobe Ruado <insidiae423@gmail.com>
2023-12-29 20:31:51 +0800

    🧝‍♂️ Setup Exercise 6.2

--- epic-notes/app/utils/misc.tsx
+++ epic-notes/app/utils/misc.tsx
@@ -6,11 +6,11 @@ import { twMerge } from "tailwind-merge";
 import userFallback from "#app/assets/user.png";
 
 export function getUserImgSrc(imageId?: string | null) {
-	return imageId ? `/resources/images/${imageId}` : userFallback;
+	return imageId ? `/resources/user-images/${imageId}` : userFallback;
 }
 
 export function getNoteImgSrc(imageId: string) {
-	return `/resources/images/${imageId}`;
+	return `/resources/note-images/${imageId}`;
 }
 
 export function getErrorMessage(error: unknown) {
