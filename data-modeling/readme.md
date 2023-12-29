0d67d844cc95ac795cacb016f46e3a07ab152c02
Kobe Ruado <insidiae423@gmail.com>
2023-12-29 19:28:57 +0800

    Finish Exercise 4.2

--- data-modeling/readme.md
+++ data-modeling/readme.md
@@ -9,20 +9,20 @@ TODO: Add notes
 ## [02. Data Relationships](./02.relationships/`)
 
 1. [One-to-Many Relationships](./02.relationships/01.one-to-many/)
-2. One-to-One Relationships
+2. [One-to-One Relationships](./02.relationships/02.one-to-one/)
 
 TODO: Add notes
 
-## 03. Data Migrations
+## [03. Data Migrations](./03.migrations/)
 
-1. Migrations
+1. [Migrations](./03.migrations/01.migrations/)
 
 TODO: Add notes
 
-## 04. Seeding Data
+## [04. Seeding Data](./04.seed/)
 
-1. Init Seed Script
-2. Nested Writes
+1. [Init Seed Script](./04.seed/01.init/)
+2. [Nested Writes](./04.seed/02.nested/)
 
 TODO: Add notes
 
--- epic-notes/prisma/seed.ts
+++ epic-notes/prisma/seed.ts
@@ -5,36 +5,36 @@ const prisma = new PrismaClient();
 
 await prisma.user.deleteMany();
 
-const kody = await prisma.user.create({
+await prisma.user.create({
 	data: {
 		email: "kody@kcd.dev",
 		username: "kody",
 		name: "Kody",
-	},
-});
-
-await prisma.note.create({
-	data: {
-		id: "d27a197e",
-		title: "Basic Koala Facts",
-		content:
-			"Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!",
-		ownerId: kody.id,
-		images: {
+		notes: {
 			create: [
 				{
-					altText: "an adorable koala cartoon illustration",
-					contentType: "image/png",
-					blob: await fs.promises.readFile(
-						"./tests/fixtures/images/kody-notes/cute-koala.png",
-					),
-				},
-				{
-					altText: "a cartoon illustration of a koala in a tree eating",
-					contentType: "image/png",
-					blob: await fs.promises.readFile(
-						"./tests/fixtures/images/kody-notes/koala-eating.png",
-					),
+					id: "d27a197e",
+					title: "Basic Koala Facts",
+					content:
+						"Koalas are found in the eucalyptus forests of eastern Australia. They have grey fur with a cream-coloured chest, and strong, clawed feet, perfect for living in the branches of trees!",
+					images: {
+						create: [
+							{
+								altText: "an adorable koala cartoon illustration",
+								contentType: "image/png",
+								blob: await fs.promises.readFile(
+									"./tests/fixtures/images/kody-notes/cute-koala.png",
+								),
+							},
+							{
+								altText: "a cartoon illustration of a koala in a tree eating",
+								contentType: "image/png",
+								blob: await fs.promises.readFile(
+									"./tests/fixtures/images/kody-notes/koala-eating.png",
+								),
+							},
+						],
+					},
 				},
 			],
 		},
