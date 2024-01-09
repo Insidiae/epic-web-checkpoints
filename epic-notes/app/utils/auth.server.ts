45281608507f95235ac287a08a46310bcd8b21c8
Kobe Ruado <insidiae423@gmail.com>
2024-01-09 13:31:33 +0800

    Finish Exercise 6.2

--- epic-notes/app/utils/auth.server.ts
+++ epic-notes/app/utils/auth.server.ts
@@ -1,3 +1,8 @@
 import bcrypt from "bcryptjs";
 
 export { bcrypt };
+
+const SESSION_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 30; // 30 days
+export function getSessionExpirationDate() {
+	return new Date(Date.now() + SESSION_EXPIRATION_TIME);
+}
