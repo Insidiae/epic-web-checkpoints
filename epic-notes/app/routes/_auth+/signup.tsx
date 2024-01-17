7d44404ff84a587ecc23bc801d3d91c4c05a8615
Kobe Ruado <insidiae423@gmail.com>
2024-01-18 02:14:38 +0800

    🧝‍♂️ Setup Exercise 10.3

--- epic-notes/app/routes/_auth+/signup.tsx
+++ epic-notes/app/routes/_auth+/signup.tsx
@@ -10,51 +10,21 @@ import {
 import { Form, useActionData, useSearchParams } from "@remix-run/react";
 import { AuthenticityTokenInput } from "remix-utils/csrf/react";
 import { HoneypotInputs } from "remix-utils/honeypot/react";
-import { safeRedirect } from "remix-utils/safe-redirect";
 import { z } from "zod";
-import { CheckboxField, ErrorList, Field } from "#app/components/forms.tsx";
-import { Spacer } from "#app/components/spacer.tsx";
+import { GeneralErrorBoundary } from "#app/components/error-boundary.tsx";
+import { ErrorList, Field } from "#app/components/forms.tsx";
 import { StatusButton } from "#app/components/ui/status-button.tsx";
-import {
-	requireAnonymous,
-	signup,
-	sessionKey,
-} from "#app/utils/auth.server.ts";
+import { requireAnonymous } from "#app/utils/auth.server.ts";
 import { validateCSRF } from "#app/utils/csrf.server.ts";
 import { prisma } from "#app/utils/db.server.ts";
 import { checkHoneypot } from "#app/utils/honeypot.server.ts";
 import { useIsPending } from "#app/utils/misc.tsx";
-import { sessionStorage } from "#app/utils/session.server.ts";
-import {
-	EmailSchema,
-	NameSchema,
-	PasswordSchema,
-	UsernameSchema,
-} from "#app/utils/user-validation.ts";
+import { EmailSchema } from "#app/utils/user-validation.ts";
 
-const SignupFormSchema = z
-	.object({
-		username: UsernameSchema,
-		name: NameSchema,
-		email: EmailSchema,
-		password: PasswordSchema,
-		confirmPassword: PasswordSchema,
-		agreeToTermsOfServiceAndPrivacyPolicy: z.boolean({
-			required_error:
-				"You must agree to the terms of service and privacy policy",
-		}),
-		remember: z.boolean().optional(),
-		redirectTo: z.string().optional(),
-	})
-	.superRefine(({ confirmPassword, password }, ctx) => {
-		if (confirmPassword !== password) {
-			ctx.addIssue({
-				path: ["confirmPassword"],
-				code: "custom",
-				message: "The passwords must match",
-			});
-		}
-	});
+const SignupSchema = z.object({
+	email: EmailSchema,
+	redirectTo: z.string().optional(),
+});
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	await requireAnonymous(request);
@@ -67,22 +37,19 @@ export async function action({ request }: ActionFunctionArgs) {
 	await validateCSRF(formData, request.headers);
 	checkHoneypot(formData);
 	const submission = await parse(formData, {
-		schema: SignupFormSchema.superRefine(async (data, ctx) => {
+		schema: SignupSchema.superRefine(async (data, ctx) => {
 			const existingUser = await prisma.user.findUnique({
-				where: { username: data.username },
+				where: { email: data.email },
 				select: { id: true },
 			});
 			if (existingUser) {
 				ctx.addIssue({
-					path: ["username"],
+					path: ["email"],
 					code: z.ZodIssueCode.custom,
-					message: "A user already exists with this username",
+					message: "A user already exists with this email",
 				});
 				return;
 			}
-		}).transform(async data => {
-			const session = await signup(data);
-			return { ...data, session };
 		}),
 		async: true,
 	});
@@ -90,28 +57,28 @@ export async function action({ request }: ActionFunctionArgs) {
 	if (submission.intent !== "submit") {
 		return json({ status: "idle", submission } as const);
 	}
-	if (!submission.value?.session) {
+	if (!submission.value?.email) {
 		return json({ status: "error", submission } as const, { status: 400 });
 	}
+	const { email } = submission.value;
 
-	const { session, remember, redirectTo } = submission.value;
+	// TODO: We'll implement this later!
+	const response = {
+		status: "error",
+		error: `this has not yet been implemented ${email}`,
+	};
 
-	const cookieSession = await sessionStorage.getSession(
-		request.headers.get("cookie"),
-	);
-	cookieSession.set(sessionKey, session.id);
-
-	return redirect(safeRedirect(redirectTo), {
-		headers: {
-			"set-cookie": await sessionStorage.commitSession(cookieSession, {
-				expires: remember ? session.expirationDate : undefined,
-			}),
-		},
-	});
+	if (response.status === "success") {
+		// we'll handle this soon...
+		return redirect("/onboarding");
+	} else {
+		submission.error[""] = [response.error];
+		return json({ status: "error", submission } as const, { status: 500 });
+	}
 }
 
 export const meta: MetaFunction = () => {
-	return [{ title: "Setup Epic Notes Account" }];
+	return [{ title: "Sign Up | Epic Notes" }];
 };
 
 export default function SignupRoute() {
@@ -123,116 +90,54 @@ export default function SignupRoute() {
 
 	const [form, fields] = useForm({
 		id: "signup-form",
-		constraint: getFieldsetConstraint(SignupFormSchema),
+		constraint: getFieldsetConstraint(SignupSchema),
 		defaultValue: { redirectTo },
 		lastSubmission: actionData?.submission,
 		onValidate({ formData }) {
-			return parse(formData, { schema: SignupFormSchema });
+			const result = parse(formData, { schema: SignupSchema });
+			return result;
 		},
 		shouldRevalidate: "onBlur",
 	});
 
 	return (
-		<div className="container flex min-h-full flex-col justify-center pb-32 pt-20">
-			<div className="mx-auto w-full max-w-lg">
-				<div className="flex flex-col gap-3 text-center">
-					<h1 className="text-h1">Welcome aboard!</h1>
-					<p className="text-body-md text-muted-foreground">
-						Please enter your details.
-					</p>
-				</div>
-				<Spacer size="xs" />
-				<Form
-					method="POST"
-					className="mx-auto min-w-[368px] max-w-sm"
-					{...form.props}
-				>
+		<div className="container flex flex-col justify-center pb-32 pt-20">
+			<div className="text-center">
+				<h1 className="text-h1">Let's start your journey!</h1>
+				<p className="mt-3 text-body-md text-muted-foreground">
+					Please enter your email.
+				</p>
+			</div>
+			<div className="mx-auto mt-16 min-w-[368px] max-w-sm">
+				<Form method="POST" {...form.props}>
 					<AuthenticityTokenInput />
 					<HoneypotInputs />
 					<Field
-						labelProps={{ htmlFor: fields.email.id, children: "Email" }}
-						inputProps={{
-							...conform.input(fields.email),
-							autoComplete: "email",
-							autoFocus: true,
-							className: "lowercase",
+						labelProps={{
+							htmlFor: fields.email.id,
+							children: "Email",
 						}}
+						inputProps={{ ...conform.input(fields.email), autoFocus: true }}
 						errors={fields.email.errors}
-					/>
-					<Field
-						labelProps={{ htmlFor: fields.username.id, children: "Username" }}
-						inputProps={{
-							...conform.input(fields.username),
-							autoComplete: "username",
-							className: "lowercase",
-						}}
-						errors={fields.username.errors}
-					/>
-					<Field
-						labelProps={{ htmlFor: fields.name.id, children: "Name" }}
-						inputProps={{
-							...conform.input(fields.name),
-							autoComplete: "name",
-						}}
-						errors={fields.name.errors}
-					/>
-					<Field
-						labelProps={{ htmlFor: fields.password.id, children: "Password" }}
-						inputProps={{
-							...conform.input(fields.password, { type: "password" }),
-							autoComplete: "new-password",
-						}}
-						errors={fields.password.errors}
-					/>
-
-					<Field
-						labelProps={{
-							htmlFor: fields.confirmPassword.id,
-							children: "Confirm Password",
-						}}
-						inputProps={{
-							...conform.input(fields.confirmPassword, { type: "password" }),
-							autoComplete: "new-password",
-						}}
-						errors={fields.confirmPassword.errors}
-					/>
-
-					<CheckboxField
-						labelProps={{
-							htmlFor: fields.agreeToTermsOfServiceAndPrivacyPolicy.id,
-							children:
-								"Do you agree to our Terms of Service and Privacy Policy?",
-						}}
-						buttonProps={conform.input(
-							fields.agreeToTermsOfServiceAndPrivacyPolicy,
-							{ type: "checkbox" },
-						)}
-						errors={fields.agreeToTermsOfServiceAndPrivacyPolicy.errors}
-					/>
-					<CheckboxField
-						labelProps={{
-							htmlFor: fields.remember.id,
-							children: "Remember me",
-						}}
-						buttonProps={conform.input(fields.remember, { type: "checkbox" })}
-						errors={fields.remember.errors}
 					/>
 
 					<input {...conform.input(fields.redirectTo, { type: "hidden" })} />
 					<ErrorList errors={form.errors} id={form.errorId} />
 
-					<div className="flex items-center justify-between gap-6">
-						<StatusButton
-							className="w-full"
-							status={isPending ? "pending" : actionData?.status ?? "idle"}
-							type="submit"
-							disabled={isPending}
-						>
-							Create an account
-						</StatusButton>
-					</div>
+					<StatusButton
+						className="w-full"
+						status={isPending ? "pending" : actionData?.status ?? "idle"}
+						type="submit"
+						disabled={isPending}
+					>
+						Submit
+					</StatusButton>
 				</Form>
 			</div>
 		</div>
 	);
+}
+
+export function ErrorBoundary() {
+	return <GeneralErrorBoundary />;
 }
